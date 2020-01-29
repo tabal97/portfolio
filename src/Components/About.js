ce70f81e372ec2645fd3bac42c7ab27ac0975d6e
Mohamed <mr.tabal@yahoo.co.uk>
2020-01-29 18:22:26 +0000

    waffling in about me (too lazy to right it now)

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class About extends Component {
     state = {}
     render() {
-        return (<div><h1>About me</h1></div>);
+        return (<div>
+            <h1>About me</h1>
+            <p>
+                As thrilling as my life may be, you're gonna have to give me a bit of time to work on this section.
+        </p>
+            <hr />
+            <p>fingers crossed i'll complete this section before the beginning of march</p>
+        </div>);
     }
 }
 
