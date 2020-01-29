050552356a10490e7af3bf36efd14b04b61e700f
Mohamed <mr.tabal@yahoo.co.uk>
2020-01-29 18:22:29 +0000

    waffling in about me (too lazy to right it now)

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,7 +9,7 @@ class About extends Component {
                 As thrilling as my life may be, you're gonna have to give me a bit of time to work on this section.
         </p>
             <hr />
-            <p>fingers crossed i'll complete this section before the beginning of march</p>
+            <p>fingers crossed i'll complete this section before the beginning of march (feeling a bit lazy rn)</p>
         </div>);
     }
 }
