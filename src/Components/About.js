2b9f62646c33794dda943d05c9e80ef58f543e9b
Mohamed <mr.tabal@yahoo.co.uk>
2020-02-08 14:45:20 +0000

    fill in 'about me ' section

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,13 +3,18 @@ import React, { Component } from 'react';
 class About extends Component {
     state = {}
     render() {
-        return (<div>
+        return (<div className="about-body">
             <h1>About me</h1>
             <p>
-                As thrilling as my life may be, you're gonna have to give me a bit of time to work on this section.
-        </p>
+                Before learning to code, I’ve always had a passion for problem solving; and this was the reason that led me towards seeking a career path in Petroleum and Mechanical Engineering. During my time at university I learned the importance of managing both my time and workflow, especially whilst having to complete several projects that needed to be submitted at a certain level of quality under a deadline. It also helped me hone the skill of problem solving, where we were given tasks to solve complex problems and it really made me appreciate the art even more.
+<hr />
+                I had a very strong passion for problem solving and I quickly came to realise that a lot of the jobs in the Oil and Gas sector were not engaging and did not involve much innovation or problem solving. Things like checking pressure gauges and measuring levels of corrosion in pipes did not interest me in the slightest. At that point in my life I needed to make a decision of whether to continue in this path or choose something different that would quench my desire to create and innovate.
+<hr />
+                I decided to partake in a Full Stack development bootcamp called “Northcoders” that taught Node.js and React as its two core frameworks and also taught mySQL as part of the course. Before beginning the course, I completed an entry challenge test to check for competence and whether I was ready to complete the course in the timeframe. After passing this stage, I felt the peace of mind knowing I was ready to learn, and I made the commitment there and then.
             <hr />
-            <p>fingers crossed i'll complete this section before the beginning of march (feeling a bit lazy rn)</p>
+                My 12 weeks at Northcoders were tough, but my desire to improve and learn more pushed me to complete the course. I came out of the course with two projects; a reddit style website that I produced the database, API and frontend for; as well as a polished multiplayer app that involved players competing with one another for the closest guess of their random location. I was introduced to agile methodologies, standups and kanban boards; I was also taught paired programming and the importance of both the driver and navigator roles.
+
+            </p>
         </div>);
     }
 }
