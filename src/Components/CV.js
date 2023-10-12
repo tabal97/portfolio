acc539689b198a7ae1ddec9b406f95d756fed480
Mohamed El Tabal <53010396+tabal97@users.noreply.github.com>
2023-10-12 20:38:06 +0100

    Update cv share link

--- src/Components/CV.js
+++ src/Components/CV.js
@@ -9,7 +9,7 @@ class CV extends Component {
                 <img src={Avatar} alt="avatar" style={{ height: "200px" }} />
                 <h2 style={{ paddingTop: "2rem" }}>Mohamed El Tabal</h2>
                 <h4>Mid Level Software Developer</h4>
-                <a href="https://drive.google.com/file/d/1yPncF_VTMYERQZoE28yv2TV38aU6ccYr/view?usp=sharing" target="_blank" rel="noopener noreferrer" >
+                <a href="https://drive.google.com/file/d/19043yU64iCK71uJ0E1YV2UOfMzS67gW9/view?usp=sharing" target="_blank" rel="noopener noreferrer" >
                     <h4 style={{ color: "grey" }}>Access my CV</h4>
                 </a>
             </div>
